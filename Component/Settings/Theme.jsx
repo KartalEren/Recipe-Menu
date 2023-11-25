b41e5c315fc3539bfad04e8376057a179421e5b7
KartalEren <130454405+KartalEren@users.noreply.github.com>
2023-11-25 08:33:21 +0300

    user card added

--- Component/Settings/Theme.jsx
+++ Component/Settings/Theme.jsx
@@ -24,17 +24,27 @@ export const Theme = () => {
   }, []);
   return (
     <div className="theme">
-      <button className="button-theme" onClick={toggleTheme}>
-        {" "}
-        {theme === "light" ? "Switch to Dark" : "Switch to Light"}{" "}
-      </button>
-      <div className="user-profile">
-        <img src={user.avatar} alt="" />
-        <p>
-          Welcome <strong>{user.name}</strong>
-        </p>
-        <p>Role: {user.role}</p>
-        <p>Emal: {user.email}</p>
+      <div>
+        <button className="button-theme" onClick={toggleTheme}>
+          {" "}
+          {theme === "light" ? "Switch to Dark" : "Switch to Light"}{" "}
+        </button>
+      </div>
+      <div class="container">
+        <div class="row my-3">
+          <div class="col-md-4 mb-3">
+            <div class="user-card">
+              <p className="name">
+                Welcome <strong>{user.name}</strong>
+              </p>
+              <hr className="underline" />
+              <img className="user-image" src={user.avatar} alt="" />
+              <hr className="underline" />
+              <p className="role">Role: {user.role}</p>
+              <p className="email">Email: {user.email}</p>
+            </div>{" "}
+          </div>
+        </div>
       </div>
     </div>
   );
